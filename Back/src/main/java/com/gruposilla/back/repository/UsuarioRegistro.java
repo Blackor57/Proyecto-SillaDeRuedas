4933c5a086415fbe49ee35db466686713a550ad6
Jhoel Alonso <jtinipuclla2@gmail.com>
2025-05-12 20:34:17 -0500

    hola

--- Back/src/main/java/com/gruposilla/back/repository/UsuarioRegistro.java
+++ Back/src/main/java/com/gruposilla/back/repository/UsuarioRegistro.java
@@ -1,6 +1,7 @@
 package com.gruposilla.back.repository;
 
+import com.gruposilla.back.model.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioRegistro extends JpaRepository<UsuarioRegistro, long> {
+public interface UsuarioRegistro extends JpaRepository<Usuario, Long> {
 }
